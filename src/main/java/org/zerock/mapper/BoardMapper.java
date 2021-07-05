bf544caa1c24a9c48a0e76ad54c2d4cb1c695510
osye1128 <osye1128@naver.com>
2021-07-05 16:45:27 +0900

    first commit

---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+	//@Select("select *from tb1_board where bno>0")
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
+}
